30b961dadb8c5a8b7299b7bccfc642872c78a4e0
KellyChen <matijueming@gmail.com>
2025-01-07 12:24:15 +0800

    Initial Commit

---
+++ AnimatedSet/AnimatedSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSetApp.swift
+//  AnimatedSet
+//
+//  Created by Jia Chen on 2025/1/7.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
